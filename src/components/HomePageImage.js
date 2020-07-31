b884ca6b53ccc6d88287dc7ebd7d40f90f094fa1
Csillag <krisztian.csillag93@gmail.com>
2020-07-31 14:31:03 +0200

    style

--- src/components/HomePageImage.js
+++ src/components/HomePageImage.js
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function Homepage() {
   return (
-    <div>
+    <div className="stuff">
       <div className="row">
         <div className="column">
           <img
+            className="face"
             src="https://ca.slack-edge.com/T0DK39WAJ-UQ4BPUP7A-59982de3511b-512"
             alt="groom"
           ></img>
@@ -19,7 +20,8 @@ export default function Homepage() {
         </div>
         <div className="column">
           <img
-            src="https://onesolutionevent.com/wp-content/uploads/2016/02/unknown-person.png"
+            className="face"
+            src="https://i.ibb.co/nCCFvNq/IMG-6009-2.jpg"
             alt="Forest"
           ></img>
         </div>
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import HomePageImage from "../../components/HomePageImage";
 import LanguageChange from "../../components/LanguageChange";
+import { Jumbotron } from "react-bootstrap";
 export default function HomePage() {
   return (
     <div>
+      <img
+        className="timerPicture"
+        src="https://images-ext-2.discordapp.net/external/6_r94cK3LW5W9O9vpojgd2CxvzVrpVjbYLF6H4tF4bI/https/i.ibb.co/m0f80Pv/3450.jpg?width=1386&height=499"
+      ></img>
+
       <LanguageChange />
       <HomePageImage />
     </div>
