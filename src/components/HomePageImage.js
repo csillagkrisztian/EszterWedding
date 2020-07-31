097c21a6df3a9a9966f9495bbeb181a3c6ac54a9
Csillag <krisztian.csillag93@gmail.com>
2020-07-31 13:55:01 +0200

    survived first dem

---
+++ src/components/HomePageImage.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div>
+      <div className="row">
+        <div className="column">
+          <img
+            src="https://ca.slack-edge.com/T0DK39WAJ-UQ4BPUP7A-59982de3511b-512"
+            alt="groom"
+          ></img>
+        </div>
+        <div className="column">
+          <img
+            className="heart"
+            src="https://www.pngfind.com/pngs/m/6-62455_instagram-heart-emoji-free-download-transparent-heart-symbol.png"
+            alt="heart"
+          ></img>
+        </div>
+        <div className="column">
+          <img
+            src="https://onesolutionevent.com/wp-content/uploads/2016/02/unknown-person.png"
+            alt="Forest"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HomePageImage from "../../components/HomePageImage";
+import LanguageChange from "../../components/LanguageChange";
+export default function HomePage() {
+  return (
+    <div>
+      <LanguageChange />
+      <HomePageImage />
+    </div>
+  );
+}
